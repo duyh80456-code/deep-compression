569fbb6ab60540fc79cf8942ff9a4723bf095f4b
duyh80456-code <duyh80456@gmail.com>
2025-09-06 18:59:35 +0700

    update algo

--- pruners/__init__.py
+++ pruners/__init__.py
@@ -7,7 +7,14 @@ __all__ = ["L1Pruner", "get_pruner"]
 
 
 def get_pruner(pruner, pruning_type, **kwargs):
+    """
+    Factory trả về đúng pruner theo tên.
 
+    Args:
+        pruner (str): Tên pruner, ví dụ "L1Pruner".
+        pruning_type (str): Loại pruning, ví dụ "unstructured".
+        **kwargs: Các tham số khác (vd. init_alpha, init_delta) sẽ truyền vào L1Pruner nếu cần.
+    """
     if pruner == "L1Pruner":
         return L1Pruner(pruning_type=pruning_type, **kwargs)
     else:
